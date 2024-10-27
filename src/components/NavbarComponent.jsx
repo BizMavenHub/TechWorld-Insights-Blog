ca7aa1b120d8a4e952010c500cff7c3ef3fbd7a6
BizMavenHub <bizmavenhub@gmail.com>
2024-10-27 18:37:59 +0700

    change from anchor to Link

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
 import { Shield, Search, User, ChevronDown } from "lucide-react";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -18,7 +17,7 @@ export default function NavbarComponent() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo and site name */}
-          <a href="/" className="flex-shrink-0 flex items-center">
+          <Link href="/" className="flex-shrink-0 flex items-center">
             <img
               src="./techworld-logo.png"
               alt="icon image of techworld insights"
@@ -27,34 +26,34 @@ export default function NavbarComponent() {
             <span className="ml-2 text-xl font-bold text-gray-900">
               TechWorld Insight
             </span>
-          </a>
+          </Link>
 
           {/* Navigation links */}
           <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-            <a
+            <Link
               href="/"
               className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1  text-sm font-medium"
             >
               Home
-            </a>
-            <a
+            </Link>
+            <Link
               href="/articles"
               className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1  text-sm font-medium"
             >
               Articles
-            </a>
-            <a
+            </Link>
+            <Link
               href="/about-us"
               className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1  text-sm font-medium"
             >
               About
-            </a>
-            <a
+            </Link>
+            <Link
               href="/contact"
               className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1  text-sm font-medium"
             >
               Contact
-            </a>
+            </Link>
           </div>
 
           {/* User account dropdown */}
@@ -83,18 +82,18 @@ export default function NavbarComponent() {
             </div>
           ) : (
             <div className="flex items-center gap-4">
-              <a
+              <Link
                 href="/login"
                 className="flex items-center text-sm text-gray-700 hover:text-gray-800 border-indigo-500 border-2 hover:bg-indigo-50 h-10 px-5 rounded-lg"
               >
                 Login
-              </a>
-              <a
+              </Link>
+              <Link
                 href="/sign-up"
                 className="flex items-center text-sm text-white bg-indigo-500 hover:bg-indigo-700 px-5 h-10 rounded-lg"
               >
                 Sign Up
-              </a>
+              </Link>
             </div>
           )}
         </div>
