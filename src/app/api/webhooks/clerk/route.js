7ea71ff918c378ef4cbd43ff34407268e29629ba
TepChoulong <tepchoulongzin@gmail.com>
2024-10-29 16:38:51 +0700

    debugging

--- src/app/api/webhooks/clerk/route.js
+++ src/app/api/webhooks/clerk/route.js
@@ -5,11 +5,10 @@ import { headers } from "next/headers";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 
 export async function POST(req) {
+  console.log(process.env.STATUS);
+
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET =
-    process.env.STATUS === "development"
-      ? process.env.WEBHOOK_SECRET_DEVELOPMENT
-      : process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
